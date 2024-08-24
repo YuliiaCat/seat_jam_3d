2aaa5d75804dd9a7e528dc34f6b8249aa353cfbf
Yulia Berezniak <kotyuliya24@gmail.com>
2024-08-24 16:10:23 -0400

    links and backdrop fixed

--- src/js/burger.js
+++ src/js/burger.js
@@ -2,11 +2,19 @@ document.addEventListener('DOMContentLoaded', () => {
   const btn = document.querySelector('.header-burger');
   const menu = document.querySelector('.menu');
 
+  const toggleMenu = () => {
+    menu.classList.toggle('open');
+  };
+
   if (btn && menu) {
-    btn.addEventListener('click', () => {
-      menu.classList.toggle('open');
-    });
+    btn.addEventListener('click', toggleMenu);
   } else {
     console.error('Button or menu not found');
   }
+
+  document.addEventListener('click', (event) => {
+    if (menu.classList.contains('open') && !menu.contains(event.target) && !btn.contains(event.target)) {
+      menu.classList.remove('open');
+    }
+  });
 });
